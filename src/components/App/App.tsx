24ff6cdd79c02ddc9393532393f7c27cc4c4cd51
Volodimir <sanin.vova200@gmail.com>
2025-09-05 19:25:25 +0300

    make http resonse and form

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,55 +1,44 @@
 import { useState } from 'react'
 import css from './App.module.css'
-import CafeInfo from '../CafeInfo/CafeInfo.tsx'
-import type { Votes ,VoteType } from '../../types/votes.ts'
-import VoteOptions from '../VoteOptions/VoteOptions.tsx'
-import VoteStats from '../VoteStats/VoteStats.tsx'
-import Notification from '../Notification/Notification.tsx'
+import fetchMovies from '../../servieces/movieService.tsx'
+import SearchBar from '../SearchBar/SearchBar.tsx'
+import MovieGrid from '../MovieGrid/MovieGrid'
+import Loader from '../Loader/Loader'
+import ErrorMessage from '../ErrorMessage/ErrorMessage'
+import MovieModal from '../MovieModal/MovieModal'
+import type Movie from '../../types/movie.ts'
+import { Toaster } from 'react-hot-toast';
+import toast from 'react-hot-toast';
 
 function App() {
-  const initialVotes: Votes = {
-    good: 0,
-    neutral: 0,
-    bad: 0
+  const [movies, setMovies] = useState<Movie[]>([])
+  const [loading, setLoading] = useState(false)
+
+  const handleSubmit = async(query: string) => {
+    setMovies([]);
+    setLoading(true);
+    try {
+      const response = await fetchMovies({ query });
+      console.log(response);
+
+      if (response.results.length === 0) {
+        toast.error('No movies found for your request.')
+      } else {
+        setMovies(response.results)
+      }
+    } catch (error) {
+      console.log(error);
+      setMovies([]);
+    } finally {
+      setLoading(false);
+    }
   }
-  const [votes, setVotes] = useState<Votes>(initialVotes);
-
-  function handleVote(type: VoteType) {
-    setVotes({
-      ...votes,
-      [type]: votes[type] + 1
-    })
-  }
-
-  function resetVotes() {
-    setVotes(initialVotes)
-  }
-
-  const totalVotes:number = votes.good + votes.neutral + votes.bad;
-
-  const positiveRate = totalVotes
-    ? Math.round((votes.good / totalVotes) * 100)
-    : 0
 
   return (
     <>
       <div className={css.app}>
-        <CafeInfo></CafeInfo>
-        <VoteOptions
-          onVote={handleVote}
-          onReset={resetVotes}
-          canReset={totalVotes ? true : false}>
-        </VoteOptions>
-
-        {totalVotes > 0 ? (
-          <VoteStats
-          votes={votes}
-          totalVotes={totalVotes}
-          positiveRate={positiveRate}>
-        </VoteStats>
-        ) : (
-          <Notification></Notification>
-        )}
+        <Toaster />
+        <SearchBar onSubmit={handleSubmit}></SearchBar>
       </div>
     </>
   )
